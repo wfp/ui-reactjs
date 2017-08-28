d7def566e4bfed669c7739c39395beda77c67e31
Salvatore Avanzo <salvatore.avanzo@wfp.org>
2017-08-28 11:40:37 +0200

    update deploy procedure

--- demo/deploy/templates/settings.py
+++ demo/deploy/templates/settings.py
@@ -2,20 +2,16 @@ import os
 
 from wss_ui.settings.default import *  # noqa
 
-RAVEN_CONFIG = {
-    'dsn': '{{raven_dsn}}',
-}
-
-SESSION_COOKIE_NAME = 'wss_ui_{{config_label}}'
+SESSION_COOKIE_NAME = 'wss_ui_demo'
 
 DATABASES = {
     'default': {
-        'ENGINE': {{database_default_engine}},
-        'NAME': {{database_default_name}},
-        'HOST': {{database_default_host}},
-        'PORT': {{database_default_port}},
-        'USER': {{database_default_user}},
-        'PASSWORD': {{database_default_password}}
+        'ENGINE': {{database.default.engine}},
+        'NAME': {{database.default.name}},
+        'HOST': {{database.default.host}},
+        'PORT': {{database.default.port}},
+        'USER': {{database.default.user}},
+        'PASSWORD': {{database.default.password}}
     },
 }
 
@@ -29,10 +25,6 @@ ALLOWED_HOSTS = ['*']
 DEBUG = STATIC_FILES_DEBUG = {{django_debug}}
 
 SECRET_KEY = {{django_secret_key}}
-
-ADMINS = ({{django_admins}})
-
-TIME_ZONE = {{django_time_zone}}
 
 STATIC_ROOT = '{{django_static_root}}'
 
