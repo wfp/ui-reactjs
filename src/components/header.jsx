78a9b11da22bb49c7e9a0376cbe6ba628e94e4c5
morghot <m.toselli@core41.com>
2017-09-27 14:00:47 +0200

    comment deleted, added newline at the end of file

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,36 +21,21 @@ class Header extends React.Component {
         });
     }
     render() {
-        return (<header className="wfp-header-int">
-      <div className="wfp-grid wfp-wrapper">
+        return (
+            <header className="wfp-header-int">
+              <div className="wfp-grid wfp-wrapper">
 
-        <LogoItem title={this.props.title} />
+                <LogoItem title={this.props.title} />
 
-        <div className="wfp-u-1-2 wfp-u-sm-4-8 wfp-u-lg-7-8 header--nav">
-            <button onClick={this.toggleMenu} className="header--toggle" id="js-menu-int-trigger">Menu</button>
-            <HeaderMenuGroup openTrigger={this.state.openValue}>{this.props.children}</HeaderMenuGroup>
-        </div>
-      </div>
-    </header>);
+                <div className="wfp-u-1-2 wfp-u-sm-4-8 wfp-u-lg-7-8 header--nav">
+                    <button onClick={this.toggleMenu} className="header--toggle" id="js-menu-int-trigger">Menu</button>
+                    <HeaderMenuGroup openTrigger={this.state.openValue}>{this.props.children}</HeaderMenuGroup>
+                </div>
+              </div>
+            </header>
+       );
     }
 }
-/*
-const Header = props => {
-  return (
-    <header className="wfp-header-int">
-      <div className="wfp-grid wfp-wrapper">
-
-        <LogoItem title={props.title} />
-
-        <div className="wfp-u-1-2 wfp-u-sm-4-8 wfp-u-lg-7-8 header--nav">
-            <button className="header--toggle" id="js-menu-int-trigger">Menu</button>
-            <HeaderMenuGroup>{props.children}</HeaderMenuGroup>
-        </div>
-      </div>
-    </header>
-  );
-};
-*/
 Header.propTypes = {
     title: PropTypes.string
 };
