66de1c27f07041b5216e51e1936c91500f2425a5
morghot <m.toselli@core41.com>
2017-10-05 18:37:27 +0200

    history cell and dateformat package

--- demo/src/wss_ui/site/static/project/components/request/historycell.jsx
+++ demo/src/wss_ui/site/static/project/components/request/historycell.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import dateformat from 'dateformat';
 
 const RequestHistory = props => {
-  let historyItems = props.history.map((el, index) => {
+  const { history } = props;
+  let historyItems = history.slice(0, 5).map((el, index) => {
     return (
-      <li key={index} className="wfp-grid">
+      <li
+        className="wfp-grid history-cell--list-element"
+        key={index}
+      >
         <div className="wfp-u-5-6">
-          <span className="hist_date" >{el.timestamp}</span>
-          <span>{el.user.first_name} {el.user.last_name}</span>
+          <span className="hist_date wfp-u-6-6" >{dateformat(new Date(el.timestamp), 'dddd dd mmmm yyyy')}</span>
+          <span className="wfp-u-6-6">{el.user.first_name} {el.user.last_name}</span>
         </div>
         <div className="wfp-u-1-6 message-box">
-          <a href="#"><i className="fa fa-fw fa-envelope"></i></a>
+          <a href="#"><i className="fa fa-fw fa-envelope" /></a>
         </div>
       </li>
     );
   });
   return (
 
-      <ul>
-        {historyItems}
-      </ul>
+    <ul>
+      {historyItems}
+    </ul>
 
   );
 };
--- demo/src/wss_ui/site/static/project/components/request/requesttable.jsx
+++ demo/src/wss_ui/site/static/project/components/request/requesttable.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactTable from 'react-table'
 import PropTypes from 'prop-types';
+import React from 'react';
+import ReactTable from 'react-table';
+import RequestAvailableActions from './actionscell';
+import RequestDetails from './detailscell';
 import RequestHistory from './historycell';
 import RequestStatus from './statuscell';
-import RequestDetails from './detailscell';
-import RequestAvailableActions from './actionscell';
 
 
 export default class RequestTable extends React.Component {
@@ -35,7 +35,7 @@ export default class RequestTable extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.filterString !== this.props.filterString) {
       this.dataSource(this.props.src + nextProps.filterString, this.state.data_key);
-      this.setState({filterString: nextProps.filterString})
+      this.setState({ filterString: nextProps.filterString });
     }
   }
 
@@ -43,7 +43,7 @@ export default class RequestTable extends React.Component {
 
 
     var columns = [{
-      Header: <span>REQUESTOR<i className="fa fa-fw fa-sort"></i></span>,
+      Header: <span>REQUESTOR<i className="fa fa-fw fa-sort" /></span>,
       id:1,
       accessor: 'user',
       Cell: (row) => (
@@ -51,54 +51,58 @@ export default class RequestTable extends React.Component {
         (<div> {row.value.first_name} {row.value.last_name}</div>))
 
     },
-      {
-        Header: <span>REQUEST<i className="fa fa-fw fa-sort"></i></span>,
-          id:2,
-        accessor: 'info',
-        Cell: (row) => (<a href={row.value.url}>
-              <div>{row.value.type}</div>
-              <div>{row.value.id}</div>
-            </a>)
+    {
+      Header: <span>REQUEST<i className="fa fa-fw fa-sort" /></span>,
+      id:2,
+      accessor: 'info',
+      Cell: (row) => (<a href={row.value.url}>
+        <div>{row.value.type}</div>
+        <div>{row.value.id}</div>
+      </a>)
 
-      },
-      {
-        Header: <span>HISTORY<i className="fa fa-fw fa-sort"></i></span>,
-          id:3,
-        accessor: 'history',
-        Cell: (row) => (<RequestHistory history={row.value}/>)
+    },
+    {
+      Header: <span>HISTORY<i className="fa fa-fw fa-sort" /></span>,
+      id:3,
+      accessor: 'history',
+      Cell: (row) => (<RequestHistory history={row.value} />),
+      minWidth: 200,
 
-      },
-      {
-        Header: <span>STATUS<i className="fa fa-fw fa-sort"></i></span>,
-          id:4,
-        accessor: d=>d.info.status,
-        Cell: (row) => (<RequestStatus status={row.value}/>)
+    },
+    {
+      Header: <span>STATUS<i className="fa fa-fw fa-sort" /></span>,
+      id:4,
+      accessor: d=>d.info.status,
+      Cell: (row) => (<RequestStatus status={row.value} />)
 
-      },
-      {
-        Header: <span>DETAILS<i className="fa fa-fw fa-sort"></i></span>,
-        id:5,
-          accessor: d=>d.info.details,
-        Cell: (row) => (<RequestDetails details={row.value}/>)
+    },
+    {
+      Header: <span>DETAILS<i className="fa fa-fw fa-sort" /></span>,
+      id:5,
+      accessor: d=>d.info.details,
+      Cell: (row) => (<RequestDetails details={row.value} />)
 
-      },
-        {
-            Header: <span>ACTIONS<i className="fa fa-fw fa-sort"></i></span>,
+    },
+    {
+      Header: <span>ACTIONS<i className="fa fa-fw fa-sort" /></span>,
 
-            id: 6,
-            accessor: d => d.info.actions,
-            Cell: (row) => (<RequestAvailableActions actions={row.value.others} defaultAction={row.value.default}/>)
-        }
-    ]
+      id: 6,
+      accessor: d => d.info.actions,
+      Cell: (row) => (<RequestAvailableActions
+        actions={row.value.others}
+        defaultAction={row.value.default}
+      />)
+    }
+    ];
 
     return (
       <ReactTable
-        data={this.state.requestData}
         columns={columns}
-        showPagination={false}
+        data={this.state.requestData}
         minRows={1}
         noDataText=''
-      />)
+        showPagination={false}
+      />);
   };
 };
 
@@ -107,4 +111,3 @@ RequestTable.propTypes = {
   data_key: PropTypes.string.isRequired
 
 };
-
