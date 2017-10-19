c868621bec500126a3fcae130ae2bbbadb5a8046
Salvatore Avanzo <salvatore.avanzo@wfp.org>
2017-10-19 12:46:33 +0200

    component renamed

--- src/components/wfpModalBox.jsx
+++ src/components/wfpModalBox.jsx
@@ -4,32 +4,35 @@ import PropTypes from 'prop-types';
 import WfpActionButton from './wfpActionButton';
 
 const wfpModalBox = (props) => {
-  const mainClass = classnames('modalbox');
-  const bodyClass = classnames('modalbox--description');
-  const footerClass = classnames('modalbox--footer');
-  return (
-    <div className={mainClass}>
-      {
-        props.description && (
-          <p className={bodyClass}>{props.description}</p>
-        )}
-      <div className={footerClass}>
-      {props.actions.length > 0 && props.actions.map((action, index) => (
-        <WfpActionButton
-           action={action.label}
-           key={index}
-           type={props.type}
-           />
-      ))}
-    </div>
-      </div>
-  );
+    const mainClass = classnames('modalbox');
+    const bodyClass = classnames('modalbox--description');
+    const footerClass = classnames('modalbox--footer');
+    return (
+        <div className={mainClass}>
+            {
+                props.description && (
+                    <p className={bodyClass}>{props.description}</p>
+                )
+            }
+            <div className={footerClass}>
+                {
+                    props.actions.length > 0 && props.actions.map((action, index) => (
+                        <WfpActionButton
+                            action={action.label}
+                            key={index}
+                            type={props.type}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    );
 };
 
 wfpModalBox.propTypes = {
-  actions: PropTypes.array,
-  description: PropTypes.string,
-  type: PropTypes.string
+    actions: PropTypes.array,
+    description: PropTypes.string,
+    type: PropTypes.string
 };
 
 wfpModalBox.defaultProps = { };
