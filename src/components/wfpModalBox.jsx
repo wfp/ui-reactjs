d5ddb19c1f45066162a666067be648df7d26d299
Salvatore Avanzo <salvatore.avanzo@wfp.org>
2017-10-19 11:49:54 +0200

    release 0.0.16

---
+++ src/components/wfpModalBox.jsx
@@ -0,0 +1,37 @@
+import classnames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+import WfpActionButton from './wfpActionButton';
+
+const wfpModalBox = (props) => {
+  const mainClass = classnames('modalbox');
+  const bodyClass = classnames('modalbox--description');
+  const footerClass = classnames('modalbox--footer');
+  return (
+    <div className={mainClass}>
+      {
+        props.description && (
+          <p className={bodyClass}>{props.description}</p>
+        )}
+      <div className={footerClass}>
+      {props.actions.length > 0 && props.actions.map((action, index) => (
+        <WfpActionButton
+           action={action.label}
+           key={index}
+           type={props.type}
+           />
+      ))}
+    </div>
+      </div>
+  );
+};
+
+wfpModalBox.propTypes = {
+  actions: PropTypes.array,
+  description: PropTypes.string,
+  type: PropTypes.string
+};
+
+wfpModalBox.defaultProps = { };
+
+export default wfpModalBox;
