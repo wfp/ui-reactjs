0d7f9b75f67304a4262e75f3b8df00062b898f56
Robert Gühne <robert@polyxostudios.de>
2017-12-04 15:34:21 +0100

    Error validation updated

--- src/components/forms/mainError.jsx
+++ src/components/forms/mainError.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import classNames from 'classnames'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-const MainError = ({error}) => {
-  if (error)
+const MainError = ({ error, submitFailed }) => {
+  if (error && submitFailed === true) {
     return (
       <div className='main-error'>
         <h4 className='main-error__title'>{error.title}</h4>
         {error.fields && (
           <ul className="main-error__list">
-            { error.fields.map((field, i) => <li key={i}>{field.message}</li>) }
+            { Object.keys(error.fields).map((field, i) => <li key={i}>{error.fields[field]}</li>) }
           </ul>
         )}
-      </div>)
-  else return (null)
-}
-
-MainError.propTypes = {
-    error: PropTypes.object
+      </div>);
+  }
+  else {
+    return (null);
+  }
 };
 
-export default MainError
+MainError.propTypes = {
+  error: PropTypes.object,
+  submitFailed: PropTypes.bool
+};
+
+export default MainError;
