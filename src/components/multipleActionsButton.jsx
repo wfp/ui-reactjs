133cb9a2d6adcdebfc429a2f77ba0d5235425f27
Salvatore Avanzo <salvatore.avanzo@wfp.org>
2017-12-06 11:49:41 +0100

    bug fix: multiple actions panel does not submit

--- src/components/multipleActionsButton.jsx
+++ src/components/multipleActionsButton.jsx
@@ -5,38 +5,42 @@ import { Tooltip } from 'react-tippy';
 import { tooltipStyle } from '../utils/vars';
 
 const MultipleActionsButton = (props) => {
-        return (
-            <Tooltip className="wfp-tooltip"
-                { ...tooltipStyle }
-                position="top-end"
-                trigger="click"
-                distance={20}
-                interactive={true}
-                followCursor= {false}
-                html={(
+    const defaultDescription = "A dialog box will appear with further details and a confirmation of your action."
+    return (
+        <Tooltip className="wfp-tooltip"
+            { ...tooltipStyle }
+            position="top-end"
+            trigger="click"
+            distance={20}
+            interactive={true}
+            followCursor= {false}
+            html={(
                     <div>
                         <h4 className="tooltip__title">
-                          {!props.title ? "Other Actions" : props.title}
+                            {!props.title ? "Other Actions" : props.title}
                         </h4>
-                        <p>A dialog box will appear with further details and a confirmation of your action.</p>
+                        <p>
+                            {!props.description ? defaultDescription : props.description}
+                        </p>
                         <ul className="multiaction__list">
-                          <li>
+                            <li>
                             {props.children}
-                          </li>
+                            </li>
                         </ul>
                     </div>
                 )}>
 
-                 <button className="multiaction__more wfp-btn dropdown-trigger">
-                    <i className="fa fa-fw fa-ellipsis-v" />
-                </button>
-              </Tooltip>
-        );
+            <button className="multiaction__more wfp-btn dropdown-trigger" type="button">
+                <i className="fa fa-fw fa-ellipsis-v" />
+            </button>
+        </Tooltip>
+    );
 }
 
 MultipleActionsButton.propTypes = {
     actions: PropTypes.array,
-    title: PropTypes.string
+    title: PropTypes.string,
+    description: PropTypes.string
 };
 
 MultipleActionsButton.defaultProps = {};
