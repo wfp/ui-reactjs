4f01e8375761f5f5927b2c4bca75f3341808be87
Robert Gühne <robert@polyxostudios.de>
2017-12-16 16:32:18 +0100

    RenderRadio added, WizardNav updated for custom functions

--- src/components/forms/formActions.jsx
+++ src/components/forms/formActions.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import UniversalModal from '../universalmodal/modal';
+import UniversalModalTitle from '../universalmodal/modaltitle';
+import UniversalModalContent from '../universalmodal/modalcontent';
+import UniversalModalFooter from '../universalmodal/modalfooter';
+import WfpActionButton from '../wfpActionButton';
+
+const DefaultDialogContent = (props) => {
+  return (
+    <div>
+      <UniversalModalTitle>
+        Submit Form
+      </UniversalModalTitle>
+      <UniversalModalContent>
+        <p>Are you sure you wish to confirm this action?</p>
+        <UniversalModalFooter>
+          <a
+            href="#"
+            onClick={props.closeModal}
+          >
+            Cancel
+          </a>
+          {props.submitButton}
+        </UniversalModalFooter>
+      </UniversalModalContent>
+    </div>
+  );
+};
+
+DefaultDialogContent.propTypes = {
+  submitButton: PropTypes.node
+};
+
+
 const FormActions = ({ error, submitFailed }) => {
 	return (
 		<div className="wfp-wizard wfp-wizard-actions">
-			    <button type="submit" className="wfp-btn">Save Changes</button>
-                <button type="submit" className="wfp-btn--primary">Submit</button>
+			    <WfpActionButton label="Save Form" type="secondary"/>
+
+			    <UniversalModal
+			        className="wfp-modal--small"
+			        trigger={<WfpActionButton label="Submit Form"/>}
+			        >
+			        	<DefaultDialogContent
+			        		submitButton={ 
+				            	<WfpActionButton
+					                label="Submit Form"
+				            	/>
+				            }/>
+			        </UniversalModal>
 		</div>
     );
 };
