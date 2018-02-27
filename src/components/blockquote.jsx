b31c484cc2436522a6e8c51f21f233e9a4d8587a
Robert Gühne <robert@polyxostudios.de>
2018-02-27 14:46:22 +0100

    Added tooglable content to blockquote

--- src/components/blockquote.jsx
+++ src/components/blockquote.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Blockquote = (props) => {
+class Blockquote extends React.Component {
 
-    const { className, children, type } = props;
-    const blockquoteClass = classNames({
-      'blockquote' : true,
-      'blockquote--light': type === "light",
-      'blockquote--warning': type === "warning",
-      'blockquote--info': type === "info",
-      [`${className}`]: className
-    }); 
-    return (
-  		<div
-        {...props}
-        className={blockquoteClass}
-	    >{ children }
-	    </div>
-	  );
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleBlockquote = () => {
+    this.setState({open: !this.state.open})
+  }
+
+  render () {
+      const { className, children, style, title, toggleable, columnCount, type, ...other } = this.props;
+      const blockquoteClass = classNames({
+        'blockquote' : true,
+        'blockquote--toggleable': toggleable === true,
+        'blockquote--light': type === "light",
+        'blockquote--warning': type === "warning",
+        'blockquote--info': type === "info",
+        'blockquote--open': this.state.open
+      }); 
+
+      const blockquoteContentClass = classNames({
+        'blockquote__content' : true,
+        [`${className}`]: className
+      }); 
+
+      return (
+    		<div
+          className={blockquoteClass}
+  	    >
+          {title &&
+            <div onClick={this.toggleBlockquote} className="blockquote__title">
+              { title }
+            </div>
+          }
+          <div className={blockquoteContentClass} style={style}>
+            { children }
+          </div>
+  	    </div>
+  	  );
+    };
+}
 
 Blockquote.propTypes = {
   children: PropTypes.node,
