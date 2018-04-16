d331cd85f314ff46bfc3f755b6bc07d67965e48b
Dmitry Mashoshin <dmitry.mashoshin@anvileight.com>
2018-04-16 11:55:45 +0300

    Add month picker

---
+++ src/components/forms/monthPicker/MonthPicker.js
@@ -0,0 +1,84 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import Picker from "react-month-picker";
+import InlineError from '../inlineError';
+import moment from 'moment';
+import Label from '../label';
+
+class MonthPicker extends Component {
+  handleAMonthChange = (year, month) => {
+    const {
+      input: {onChange}
+    } = this.props;
+    const momentValue = moment().set({year, month: month - 1, day: 1});
+    onChange(momentValue)
+  };
+
+  handleClickMonthBox = () => {
+    this.container.show();
+  };
+
+  render() {
+    const {
+      id, disabled, readOnly, displayFormat, minYear, maxYear, isOutsideRange,
+      input: {
+        value = null
+      }
+    } = this.props;
+    let displayValue, mvalue;
+
+    if (value) {
+      const momentValue = moment(value);
+      displayValue = momentValue.format(displayFormat);
+      mvalue = {
+        month: momentValue.month() + 1,
+        year: momentValue.year(),
+      }
+    } else {
+      displayValue = "";
+      mvalue = {
+        month: null,
+        year: moment().year(),
+      }
+    }
+
+    return (
+      <InlineError {...this.props}>
+        <Label {...this.props} />
+        <div className="wfp-form--month-picker">
+          {readOnly === true ? (
+            <div className="">{displayValue}</div>
+          ) : (
+            <Picker
+              ref={ref => this.container = ref}
+              years={{min: minYear, max: maxYear}}
+              value={mvalue}
+              onChange={this.handleAMonthChange}
+              isOutsideRange={isOutsideRange}
+            >
+              <input
+                onClick={this.handleClickMonthBox}
+                value={displayValue}
+                disabled={disabled}
+                type="text"
+                id={id}
+                placeholder="Select month"
+              />
+            </Picker>
+          )}
+        </div>
+      </InlineError>
+    );
+  }
+}
+
+MonthPicker.propTypes = {};
+
+MonthPicker.defaultProps = {
+  displayFormat: 'MMM. YYYY',
+  id: 'month',
+  maxYear: 2100,
+  minYear: 1971
+};
+
+export default MonthPicker;
--- stories/helpers.jsx
+++ stories/helpers.jsx
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import store from "./configureStore";
 import { Provider } from 'react-redux';
-
+import FormWrapper from './formWrapper';
 
 // decorators
 
 export const ProviderDecorator = story => <Provider store={store}>{story()}</Provider>;
+export const withSimpleForm = story =>  <Provider store={store}><FormWrapper>{story()}</FormWrapper></Provider>;
 
 
 // helpers
